7575b8176d9baeeb2baea557697404dd4dc0db81
Armando Siqueiros <aikdvs@gmail.com>
2023-01-30 07:34:04 -0700

    Add files via upload

---
+++ Carrito.js
@@ -0,0 +1,22 @@
+
+Vue.createApp({
+    data() {
+        return {
+            productos: [],
+            nombre: '',
+            precio: '',
+            cantidad: '',
+            colorTable:'primary'
+        }
+    },
+    methods: {
+        agregar() {
+            const producto = {
+                nombre: this.nombre,
+                precio: this.precio,
+                cantidad: this.cantidad
+            }
+            this.productos.push(producto);
+        }
+    }
+}).mount('#App')
