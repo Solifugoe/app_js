84d0b426380d35c32629516427666425c153866d
Armando Siqueiros <aikdvs@gmail.com>
2023-01-30 08:36:56 -0700

    Add files via upload

--- plantilla.js
+++ plantilla.js
@@ -1,24 +1,8 @@
 Vue.createApp({
-    data(){
-        return{
-            productos: [
-                {
-                    nombre:'Cebollas',
-                    precio: 10,
-                    cantidad: 10
-                },
-                {
-                    nombre:'Tomates',
-                    precio: 15,
-                    cantidad: 20
-                },
-                {
-                    nombre:'Uvas',
-                    precio: 20,
-                    cantidad: 100
-                },
-            ]
-
+    data() {
+        return {
+            rango: 0,
         }
-    }
-}).mount('#App')
+    },
+    
+}).mount('#app');
